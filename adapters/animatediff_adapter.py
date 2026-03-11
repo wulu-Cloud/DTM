caccee85c067af4aa31521a35cf96c8478dfe356
wulu-Cloud <315980450@qq.com>
2026-03-11 21:59:10 +0800

    Add AnimateDiff integration code

---
+++ adapters/animatediff_adapter.py
@@ -0,0 +1,14 @@
+# AnimateDiff Integration
+
+class AnimateDiff:
+    def __init__(self, config):
+        self.config = config
+
+    def run(self):
+        # Implementation of AnimateDiff processing
+        pass
+
+if __name__ == '__main__':
+    config = {}  # Add your configuration here
+    animatediff = AnimateDiff(config)
+    animatediff.run()
